65900325cd66c3a7ff5791398bac28eb8732eb39
Jonathan Drew <jonathan.drew@codeimmersives.com>
2020-06-25 14:34:15 -0400

    starting route for users has been created, can visit back to add something to route if time, next step, make route for users registering

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var usersRouter = require("./routes/users/users")
 
 var app = express();
 require("dotenv").config();
